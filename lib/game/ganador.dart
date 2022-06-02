99b0731a1e8807ea879bbc181f5f59e6bd197bc2
KikeDeAlba <78590985+KikeDeAlba@users.noreply.github.com>
2022-06-02 15:50:20 -0600

    Update ganador.dart

--- lib/game/ganador.dart
+++ lib/game/ganador.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_preguntados/game/jugadores.dart';
 
 import '../main.dart';
+import 'juego.dart';
 
 class Ganador extends StatelessWidget {
   const Ganador({Key? key}) : super(key: key);
@@ -8,12 +10,25 @@ class Ganador extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text(appTitle),
-        centerTitle: true,
-        backgroundColor: Colors.pink[900],
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text(appTitle),
+          centerTitle: true,
+          backgroundColor: Colors.pink[900],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.center,
+                child: Text(
+                  'Jugador ${turno} ha ganado el juego',
+                  style: Style,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
