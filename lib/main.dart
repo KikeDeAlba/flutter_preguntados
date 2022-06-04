1fb42093f7d4d581be3367f330c4c90884a710f4
KikeDeAlba <78590985+KikeDeAlba@users.noreply.github.com>
2022-06-03 18:39:24 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,43 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text("Preguntados"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+        child: Container(
+          decoration: ShapeDecoration(
+            shape: Border.all(
+              color: Colors.red,
+              width: 2.0,
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(15, 50, 15, 0),
+                    decoration: ShapeDecoration(
+                      shape: Border.all(
+                        color: Colors.red,
+                        width: 2.0,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Hola mundo',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
