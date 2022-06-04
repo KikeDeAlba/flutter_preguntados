61c4f72cfefb0cda42285fc56ef2e7e6e98da973
KikeDeAlba <78590985+KikeDeAlba@users.noreply.github.com>
2022-06-03 18:18:17 -0600

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_preguntados/game/jugadores.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
     ));
-
-const appTitle = '¡Preguntados!';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const portada = 'assets/titulo.png';
-    const btnText = '¡Jugar!';
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text(appTitle),
-        centerTitle: true,
-        backgroundColor: Colors.pink[900],
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-                alignment: Alignment.center,
-                child: Image(
-                    image: AssetImage(
-                  portada,
-                ))),
-            SizedBox(height: 100),
-            Container(
-                alignment: Alignment.center,
-                child: FlatButton.icon(
-                  onPressed: (() => runApp(MaterialApp(
-                        home: jugar(),
-                      ))),
-                  icon: Icon(Icons.play_circle),
-                  label: Text(
-                    btnText,
-                    style: Style,
-                  ),
-                  color: Colors.grey,
-                  padding: EdgeInsets.fromLTRB(30, 15, 30, 15),
-                )),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
