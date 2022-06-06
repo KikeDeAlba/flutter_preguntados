691e8b594e5c36d5791bc5e1baad5906fd153daa
KikeDeAlba <78590985+KikeDeAlba@users.noreply.github.com>
2022-06-06 05:58:48 -0600

    fondito

--- lib/widgets/main/body.dart
+++ lib/widgets/main/body.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_preguntados/widgets/appBar.dart';
 
 body() {
   return Stack(children: <Widget>[
-    Image(image: AssetImage('assets/fondo.jpeg')),
-    bar(),
+    Image(
+      image: AssetImage('assets/inicio.png'),
+    ),
     Padding(
-        padding: EdgeInsets.fromLTRB(10, 90, 10, 20),
+        padding: EdgeInsets.fromLTRB(10, 130, 10, 20),
         child: Container(
-            decoration: ShapeDecoration(
-                shape: Border.all(color: Colors.black, width: 2.0)),
             child: Column(children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                  child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 20, 15, 0),
-                      decoration: ShapeDecoration(
-                          shape: Border.all(color: Colors.black, width: 2.0)),
-                      alignment: Alignment.center,
-                      child: FlatButton(
-                          onPressed: () {},
-                          height: 40,
-                          minWidth: 150,
-                          color: Colors.pinkAccent,
-                          child:
-                              Text('Jugar', style: TextStyle(fontSize: 23)))))
-            ])))
+          Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(15, 22, 15, 0),
+                  alignment: Alignment.center,
+                  child: FlatButton(
+                      onPressed: () {},
+                      height: 40,
+                      minWidth: 150,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      color: Colors.pinkAccent,
+                      child: Text('Jugar', style: TextStyle(fontSize: 23)))))
+        ])))
   ]);
 }
